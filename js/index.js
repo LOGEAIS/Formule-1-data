4c353c965bfae660a36912c34a73085a229cb322
LOGEAIS Etienne <etienne.logeais@efrei.net>
2019-05-28 16:48:31 +0200

    img/background

---
+++ js/index.js
@@ -0,0 +1,55 @@
+function defaut() 
+{
+    document.body.style.backgroundImage = "url('../img/background/haas.jpg')";
+}
+
+function bacground_ferrari() 
+{
+    document.body.style.backgroundImage = "";
+}
+
+function bacground_maclaren() 
+{
+    setTimeout(function()
+    { document.body.style.backgroundImage = "url('../img/background/mclaren.jpg')"; }, 3);
+}
+
+function bacground_haas() 
+{
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/haas.jpg')"; }, 3);
+}
+
+function bacground_mercedes() 
+{
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/mercedes.jpg')"; }, 3);
+}
+
+function bacground_force_india() 
+{
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/force_india.jpg')"; }, 3);
+}
+
+function bacground_redbull() 
+{
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/redbull.jpg')"; }, 3);
+}
+
+function bacground_renault() 
+{
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/renault.jpg')"; }, 3);
+}
+
+function bacground_torrorosso() 
+{
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/torro_rosso.jpg')"; }, 3);
+}
+
+function bacground_williams() 
+{
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/williams.jpg')"; }, 3);
+}
+
+function bacground_alpharomeo() 
+{
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/alpharomeo.jpg')"; }, 3);
+}
