29c27814a34329fb0bbc80ad65f563374012c0d8
LOGEAIS Etienne <etienne.logeais@efrei.net>
2019-06-07 13:48:13 +0200

    maj footer

--- js/index.js
+++ js/index.js
@@ -6,51 +6,51 @@ function defaut()
 function bacground_ferrari()
 {
     setTimeout(function()
-    {document.body.style.backgroundImage = "url('../img/background/ferrari.jpg')";},3);
+    {document.body.style.backgroundImage = "url('../img/background/ferrari.jpg')";});
 }
 
 function bacground_maclaren()
 {
     setTimeout(function()
-    { document.body.style.backgroundImage = "url('../img/background/mclaren.jpg')"; }, 3);
+    { document.body.style.backgroundImage = "url('../img/background/mclaren.jpg')"; });
 }
 
 function bacground_haas()
 {
-    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/haas.jpg')"; }, 3);
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/haas.jpg')"; });
 }
 
 function bacground_mercedes()
 {
-    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/mercedes.jpg')"; }, 3);
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/mercedes.jpg')"; });
 }
 
 function bacground_force_india()
 {
-    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/force_india.jpg')"; }, 3);
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/force_india.jpg')"; });
 }
 
 function bacground_redbull()
 {
-    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/redbull.jpg')"; }, 3);
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/redbull.jpg')"; });
 }
 
 function bacground_renault()
 {
-    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/renault.jpg')"; }, 3);
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/renault.jpg')"; });
 }
 
 function bacground_torrorosso()
 {
-    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/torro_rosso.jpg')"; }, 3);
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/torro_rosso.jpg')"; });
 }
 
 function bacground_williams()
 {
-    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/williams.jpg')"; }, 3);
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/williams.jpg')"; });
 }
 
 function bacground_alpharomeo()
 {
-    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/alpharomeo.jpg')"; }, 3);
+    setTimeout(function(){ document.body.style.backgroundImage = "url('../img/background/alpharomeo.jpg')"; });
 }
