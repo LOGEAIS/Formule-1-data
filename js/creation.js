f247e00dabecb0fae70e3f76f8b1bd9208757041
LOGEAIS Etienne <etienne.logeais@efrei.net>
2019-06-16 15:51:20 +0200

    indentation

--- js/creation.js
+++ js/creation.js
@@ -5,37 +5,38 @@ function block()
 
 function ajouter()
 {
-    /*if (document.getElementById('name').value == "" || document.getElementById('description').value == "")
+    if (document.getElementById('name').value == "" || document.getElementById('description').value == "")
 	{
 		alert("Erreur");
-    }*/
+    }
+    else
+    {
+        var table = document.getElementById("first");
+        var title_name = document.getElementById("title_name");
+        var info = document.getElementById("new");
+        var desc = document.getElementById("description1");
+        var birth = document.getElementById("birth1");
 
-    var table = document.getElementById("first");
-   
-    var title_name = document.getElementById("title_name");
-    var info = document.getElementById("new");
-    var desc = document.getElementById("description1");
-    var birth = document.getElementById("birth1");
+        var name = document.createElement('h2');
+        name.textContent = document.getElementById('name').value;
 
-    var name = document.createElement('h2');
-    name.textContent = document.getElementById('name').value;
+        var description = document.createElement('span');
+        description.textContent = document.getElementById('description').value;
 
-    var description = document.createElement('span');
-    description.textContent = document.getElementById('description').value;
+        var ecuries = document.createElement('span');
+        ecuries.textContent = document.getElementById('ecuries').value;
 
-    var ecuries = document.createElement('span');
-    ecuries.textContent = document.getElementById('ecuries').value;
+        var birth1 = document.createElement('span');
+        birth1.textContent = document.getElementById('birth').value;
 
-    var birth1 = document.createElement('span');
-    birth1.textContent = document.getElementById('birth').value;
+        var push = title_name.appendChild(name);
+        var push = desc.appendChild(description);
+        var push = info.appendChild(ecuries);
+        var push = birth.appendChild(birth1);
 
-    var push = title_name.appendChild(name);
-    var push = desc.appendChild(description);
-    var push = info.appendChild(ecuries);
-    var push = birth.appendChild(birth1);
-
-    document.getElementById("result").style.display = "block";
-    document.getElementById("form").style.display = "none";
+        document.getElementById("result").style.display = "block";
+        document.getElementById("form").style.display = "none";
+    }
 }
 
 function nouveau()
